612586d745665622fb8b4ec76be3e1ec5916e6a7
rafaelrodrigues86 <rafaelrs270186@gmail.com>
2023-09-06 23:34:08 -0300

    Foi adicionado mais uma linha para imprimir, para testar o commit do próprio intelliJ.

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Testando  primeiro codigo java");
+        System.out.println("linha adicionada para fazer commit");
     }
 }
